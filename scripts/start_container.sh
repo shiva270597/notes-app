e81ddb9a270e0639646241265568960133fb6ebd
shiva270597 <enugalareddy.er@gmail.com>
2024-08-06 23:31:48 +0530

    scripts

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull 8000:8000 reddy2705/notesapp1
+
+# Run the Docker image as a container
+docker run -d -p 8000:8000 reddy2705/notesapp1
---
+++ scripts/stop_container.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+# Stop the running container (if any)
+containerid= 'docker ps | awk -F" " '{Print$1}''
+docker rm -f $containerid
